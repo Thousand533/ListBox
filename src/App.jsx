5b591da042cce00c63672b7cefeeb1e2db16ecf6
YiChien <j04566704@gmail.com>
2024-09-08 20:42:52 +0800

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline"
 import TodoList from './TodoList'
 import Navbar from './NavBar'
+
+export function Index() {
+  return (
+    <>
+      <div>首頁 路徑是 /</div>
+      <img src={`${import.meta.env.VITE_BASENAME}/1.jpg`} alt="" />
+    </>
+  );
+}
+export function Page2() {
+  return (
+    <>
+      <div>Page2 路徑是 Page2</div>
+      <img src={`${import.meta.env.VITE_BASENAME}/2.jpg`} alt="" />
+    </>
+  );
+}
+
 function App() {
   return (
     <>
+      <BrowserRouter basename="/test-vite-github-pages">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">page /</Link>
+            </li>
+            <li>
+              <Link to="/page2">page2</Link>
+            </li>
+          </ul>
+        </nav>
+      </BrowserRouter>
       <Navbar />
       <CssBaseline/>
       <TodoList/>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "react-todos", //這邊換成你的專案名
+});
